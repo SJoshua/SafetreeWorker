239ebc838b957fb7acea82825795d5f867b482e6
Joshua Astray <JoshuaSRKF@gmail.com>
2017-04-09 20:07:41 +0800

    Update main.js
    Update API

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function work(id, next) {
 	if (id < tasks.length) {
 		console.log("[task] #" + id);
 		SeeVideo.SkillCheckName(tasks[id][0], tasks[id][1], tasks[id][2]);
-		var ret = SeeVideo.TemplateIn2(tasks[id][3], tasks[id][4] , tasks[id][5], "", "", "", "0|0|0", "已掌握技能", 100, 1, "", "", "", tasks[i][2]).value;
+		var ret = SeeVideo.TemplateIn2(tasks[id][3], tasks[id][4] , tasks[id][5], "", "", "", "0|0|0", "已掌握技能", 100, 1, "", "", "", tasks[id][2]).value;
 		if (ret == 1 || ret == 4) {
 			console.log("> done.")
 			return work(id + 1, next);
@@ -91,7 +91,7 @@ function work(id, next) {
 function goNext(current = 0) {
 	if (current < accounts.length) {
 		console.log("[login] " + accounts[current]);
-		$.getJSON("/jiating/LoginHandler.ashx?jsoncallback=?", { userName: accounts[current] , password: "123456", type: 'login', loginType: '2', r: Math.random() }, function (data) {
+		$.getJSON("https://fujianlogin.safetree.com.cn/LoginHandler.ashx?jsoncallback=?", { userName: accounts[current], password: "123456", checkcode: "", type: 'login', loginType: '1', r: Math.random() }, function (data) {
 			if (data.ret == 1) {
 				return work(0, current + 1);
 			} else {
